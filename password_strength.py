317d3f00b9406ef424554c3aac77891fd005d280
Pavel Kadantsev <p.a.kadantsev@gmail.com>
2017-08-22 00:05:01 +0300

    The very begining...

--- password_strength.py
+++ password_strength.py
@@ -1,8 +1,41 @@
+import argparse
+import getpass
+import re
+import os
 
+def get_bad_passwords_list(filepath):
+    if not os.path.isfile(filepath):
+        bad_passwords = []
+        return bad_passwords
+    with open(filepath, 'r', encoding = 'utf-8') as content:
+        bad_passwords = content.read()
+        return bad_passwords #str
 
 def get_password_strength(password):
-    pass
+    checks = [r'[a-z]',
+              r'[A-Z]',
+              r'\d'
+              r'[^0-9a-zA-Z]'
+              r'\S{5,}',
+              r'\S{10,}',
+              r'\S{15,}',
+
+
+
+
+
+            ]
+
+
 
 
 if __name__ == '__main__':
-    pass
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-p','--path', required = True,
+                        help = 'Enter path to file with blacklist passwords')
+    namespace = parser.parse_args()
+
+    get_bad_passwords_list(namespace.path)
+
+    #password = getpass.getpass()
+
