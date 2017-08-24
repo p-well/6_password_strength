7da0deffb490ce1ae5405bd68bf82744bbccd5a6
Pavel Kadantsev <p.a.kadantsev@gmail.com>
2017-08-24 22:30:18 +0300

    Finished

--- password_strength.py
+++ password_strength.py
@@ -3,49 +3,46 @@ import getpass
 import re
 import os
 
-
 def get_bad_passwords(filepath):
     if not os.path.isfile(filepath):
         bad_passwords = []
-        return bad_passwords
-    with open(filepath, 'r', encoding = 'utf-8') as content:
-        bad_passwords = content.read()
-        return bad_passwords #str
+    else:
+        with open(filepath, 'r', encoding = 'utf-8') as content:
+            bad_passwords = [content.read()]
+    return bad_passwords
 
 def get_password_strength(password):
-    checks = [r'[a-z]', #any lower case character
-              r'[A-Z]', #any upper case character
-              r'\d',    #any digit #r'[0-9]'
-              r'[\w\s]', #any special symbol except whitespace #r'[^0-9a-zA-Z,^\s]' or r'[^0-9a-zA-Z^\s]'
-              r'[?!(\S)\1{3,}]'
-              r'.{5,}',
-              r'\S{10,}',
-              r'\S{15,}']
+    checks = [r'[a-z]', 
+              r'[A-Z]', 
+              r'\d',    
+              r'\W', 
+              r'^(?!\S*(\S)\1{3,})',
+              r'\S{5,}',
+              r'\S{8,}',
+              r'\S{10,}']
 
     password_rate = 0
 
-    if password not in get_bad_passwords(namespace.path):
-        password_rate += 1
+    if get_bad_passwords(namespace.blacklist_filepath) and\
+    password not in get_bad_passwords(namespace.blacklist_filepath):
+        password_rate += 2
         print("Not in black list")
 
     for template in checks:
-        if re.search(template, password):
+        if re.findall(template, password):
             password_rate += 1
-            print(template, "Passed this check")
+            print(template, "Passed")
         else:
-            print(template, "Какая-то ерунда")
-
-
+            print(template, "Not passed")
 
     return password_rate
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
-    parser.add_argument('-p','--path', required = True,
-                        help = 'Enter path to file with blacklist passwords')
+    parser.add_argument('blacklist_filepath', nargs='?', default ='blacklist.txt')
     namespace = parser.parse_args()
-
-    password = input("Введи пароль")
-    #password = getpass.getpass()
-    print(get_password_strength(password))
+    password = input("Password: ")
+    #password = getpass.getpass("\nType password to chech its strength: ")
+    print("Your password is rated at {} point(s) on a 10-point scale"\
+    .format(get_password_strength(password)))
 
