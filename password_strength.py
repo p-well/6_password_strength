b64c9ad0364d1ad780a78f55aeaf31549352d01f
Pavel Kadantsev <p.a.kadantsev@gmail.com>
2017-08-24 06:09:12 +0300

    nothing new

--- password_strength.py
+++ password_strength.py
@@ -3,6 +3,7 @@ import getpass
 import re
 import os
 
+
 def get_bad_passwords(filepath):
     if not os.path.isfile(filepath):
         bad_passwords = []
@@ -14,10 +15,10 @@ def get_bad_passwords(filepath):
 def get_password_strength(password):
     checks = [r'[a-z]', #any lower case character
               r'[A-Z]', #any upper case character
-              r'\d'     #any digit #r'[0-9]'
-              r'[\w\s]' #any special symbol except whitespace #r'[^0-9a-zA-Z,^\s]' or r'[^0-9a-zA-Z^\s]'
-              #r'[(\S)\1{3,}]'
-              r'\S{5,}',
+              r'\d',    #any digit #r'[0-9]'
+              r'[\w\s]', #any special symbol except whitespace #r'[^0-9a-zA-Z,^\s]' or r'[^0-9a-zA-Z^\s]'
+              r'[?!(\S)\1{3,}]'
+              r'.{5,}',
               r'\S{10,}',
               r'\S{15,}']
 
@@ -28,9 +29,13 @@ def get_password_strength(password):
         print("Not in black list")
 
     for template in checks:
-        if re.findall(template, password):
+        if re.search(template, password):
             password_rate += 1
             print(template, "Passed this check")
+        else:
+            print(template, "Какая-то ерунда")
+
+
 
     return password_rate
 
@@ -40,7 +45,7 @@ if __name__ == '__main__':
                         help = 'Enter path to file with blacklist passwords')
     namespace = parser.parse_args()
 
-    password = input("Введи пароль:")
+    password = input("Введи пароль")
     #password = getpass.getpass()
     print(get_password_strength(password))
 
