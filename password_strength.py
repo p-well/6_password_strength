9c23d091f6e15c663dc63131844e3b9c6875fe68
Pavel Kadantsev <p.a.kadantsev@gmail.com>
2017-08-25 20:16:58 +0300

    Fixed bug with search of partial user password in blacklist

--- password_strength.py
+++ password_strength.py
@@ -8,7 +8,7 @@ def get_bad_passwords(filepath):
         bad_passwords = []
     else:
         with open(filepath, 'r', encoding = 'utf-8') as content:
-            bad_passwords = [content.read()]
+            bad_passwords = content.read().split("\n")
     return bad_passwords
 
 def get_password_strength(password):
@@ -26,23 +26,16 @@ def get_password_strength(password):
     if get_bad_passwords(namespace.blacklist_filepath) and\
     password not in get_bad_passwords(namespace.blacklist_filepath):
         password_rate += 2
-        print("Not in black list")
 
     for template in checks:
         if re.findall(template, password):
             password_rate += 1
-            print(template, "Passed")
-        else:
-            print(template, "Not passed")
-
     return password_rate
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('blacklist_filepath', nargs='?', default ='blacklist.txt')
     namespace = parser.parse_args()
-    password = input("Password: ")
-    #password = getpass.getpass("\nType password to chech its strength: ")
-    print("Your password is rated at {} point(s) on a 10-point scale"\
+    password = getpass.getpass("\nType password to check its strength: ")
+    print("\nYour password is rated at {} point(s) on a 10-point scale"\
     .format(get_password_strength(password)))
-
